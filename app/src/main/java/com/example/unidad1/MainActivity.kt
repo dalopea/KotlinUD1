58bba7e12166423ce11604b5fdaf50fd893e67be
dalopea <dalopea@unirioja.es>
2024-02-15 18:43:51 +0100

    committ

--- app/src/main/java/com/example/unidad1/MainActivity.kt
+++ app/src/main/java/com/example/unidad1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,6 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unidad1.ui.theme.Unidad1Theme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,16 +32,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+Surface(color=Color.Cyan) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hi, my name is $name!",
+        modifier = Modifier.padding(24.dp)
     )
+}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Unidad1Theme {
-        Greeting("Android")
+        Greeting("David")
     }
 }
